8b0163fa53df9da352b2c52a863e39ddfefcdc84
Clay Coleman <claycoleman94@gamil.com>
2022-08-17 23:59:31 -0400

    inital upload

---
+++ ClassesExercises/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ClassesExercises
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(string makeInput, string modelInput, int year) // so when you call this in the main class you wont have define it as its already here
+        {
+            Make = makeInput;     // in this sitaution the Make is referring to the property and make is referring to the perameter
+            Model = modelInput;
+            Year = year;
+
+        }
+
+
+        //Create a Make property of type string that is public
+
+        public string Make { get; set; } // get = read , set = write
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+
+    }
+}
+
---
+++ ClassesExercises/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System;
+using ClassesExercises;
+
+////Create a new instance of the Car class
+//Make sure you are inside of the Main() method’s scope
+//Set all of the object’s properties with a value
+//Print the values of each property to the Console
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Car myCar = new Car();  // Car(); - this is called Constructor - special member method. Helps us create a new instance of Car class
+        myCar.Make = "Buick";
+        myCar.Model = "Regal";
+        myCar.Year = 2012;
+        // ^^ this above is DOT notation way 
+
+
+
+        // Now say you wanted to do another car, you can do this and make it shorter as its in the scope of the main method:
+        var ford = new Car()   // Ford is the Object 
+        {
+            Make = "ford",  // Note that you will use Commons , instead of ;
+            Model = "F150",  
+            Year = 2020,
+
+        }; // Note the simicolon goes at the end here
+
+        // ^^ this way is Object intializer way 
+
+
+        var chevy = new Car("chevy", "Impala", 2008);
+
+        // ^^^ another way to write it
+
+        var carList = new List<Car>() { myCar, ford, chevy};  // List<Car>() --  is the Constructor
+
+        //Console.WriteLine(carList.Capacity);
+
+        foreach (var vehicle in carList)
+        {
+            Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+        }
+
+    }
+}
